2c71c51cf79491e90bc3f127f0b7679bd11d7cf3
Allen <allenantony2001@gmail.com>
2025-05-15 15:06:02 +0800

    Update to perform clean uninstall before rebuilding

--- build_parser.sh
+++ build_parser.sh
@@ -1,13 +1,24 @@
 
+# Remove old parser directory if it exists
+
+if [ -d "parser/src/bisonParser" ]; then
+    rm -rf parser/src/bisonParser
+fi
+
+cd parser/pybind
+python setup.py clean --all
+cd ../..
+
+# Build the BNFC parser and make the C library
 bnfc --c -m -o parser/src/bisonParser VNNLib_LBNF.cf && make -C parser
 
 sed -i '/#define PRINTER_HEADER/a\
 \nextern char *buf_;\
 ' parser/src/bisonParser/Printer.h
 
+# Generate the Python library
 cd parser/pybind
-
 python generate_wrappers.py
-
 python setup.py build_ext --inplace
+cd ../../
 
