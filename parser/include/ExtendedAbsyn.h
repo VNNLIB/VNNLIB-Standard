91c16746471cc978091e94842676815b7d5b2aa9
Allen <allenantony2001@gmail.com>
2025-06-11 23:51:23 +0800

    66: Create 'linearise' function for linearisation of an ArithExpr struct to the LinearArithExpr struct.
    - Create the main 'linearise' function
    - Create pretty printer function for LinearArithExpr
    - Create debug functions for applying linearisation to all the top-level ArithExpr structs in a Query and printing them using ppLinearArithExpr

---
+++ parser/include/ExtendedAbsyn.h
@@ -0,0 +1,29 @@
+#ifndef EXTENDED_ABSYN_H
+#define EXTENDED_ABSYN_H
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <stdbool.h>
+#include <math.h>
+#include "Absyn.h"
+#include "Parser.h"
+#include "Printer.h"
+#include "Util.h"
+
+struct LinearArithExpr_;
+typedef struct LinearArithExpr_ *LinearArithExpr;
+
+struct LinearArithExpr_ {
+    double *coeffs;  // Coefficients for the linear terms
+    TensorElement *vars; // Variables in the linear expression
+    int num_terms;   // Number of terms in the linear expression
+    double constant; // Constant term
+};
+
+LinearArithExpr linearise(ArithExpr arith_expr);
+
+void freeLinearArithExpr(LinearArithExpr expr);
+
+void debugQuery(Query q);
+
+#endif // EXTENDED_ABSYN_H
